d18532963f16a1072199f895e589a846506efb5b
cnodon <odon@qq.com>
2020-04-26 06:45:43 +0800

    added template inheritance

--- webmain.py
+++ webmain.py
@@ -5,10 +5,27 @@ from flask import render_template
 app = Flask(__name__)
 
 
-@app.route('/hello')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
+# @app.route('/hello')
+# @app.route('/hello/<name>')
+# def hello(name=None):
+#     return render_template('hello.html', name=name)
+
+
+@app.route("/")
+@app.route("/index")
+def index():
+    user = {'username': 'PKR'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
 
 
 @app.route("/")
